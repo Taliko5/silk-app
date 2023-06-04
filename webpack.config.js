300df6c1e34d7a839d1069ae019703c12c6a0aa1
noliko-zwa <noicomimo79@gmail.com>
2023-06-04 21:55:34 +0200

    feat: add html webpack plugin

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'app.js',
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx', '.js'],
+    // Add support for TypeScripts fully qualified ESM imports.
+    extensionAlias: {
+      '.js': ['.js', '.ts'],
+      '.cjs': ['.cjs', '.cts'],
+      '.mjs': ['.mjs', '.mts'],
+    },
+  },
+  module: {
+    rules: [
+      // all files with a `.ts`, `.cts`, `.mts` or `.tsx` extension will be handled by `ts-loader`
+      {
+        test: /\.([cm]?ts|tsx)$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: false,
+        },
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin(
+    {
+      title: 'silk'
+    }
+  )],
+
+};
