970f9df997ca5a2f1141768e68d4572722942e56
ntlico <39406525+ntlico@users.noreply.github.com>
2024-02-15 13:57:44 +0100

    --amend

--- src/components/Tile/Tile.style.tsx
+++ src/components/Tile/Tile.style.tsx
@@ -1,19 +1,4 @@
-import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
-
-export const animationFrame = keyframes`
-  0% {
-    outline-offset: 0;
-  }
-  30% {
-    outline-offset: -3px;
-    outline: 3px solid darkgrey;
-  }
-  100% {
-    outline-offset: -16px;
-    outline: 8px solid darkgrey;
-  }
-`;
 
 export const TileWrapper = styled.div<{ size: { min: number; max: number } }>`
   width: ${(props) => props.size.max}px;
